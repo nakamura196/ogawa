806e8c1a667ba26fa8644affa47a8a3445a6dd70
nakamura196 <na.kamura.1263@gmail.com>
2021-12-16 18:14:44 +0900

    feat: add edge from factoid to entity

--- pages/item/_id.vue
+++ pages/item/_id.vue
@@ -27,9 +27,20 @@
         :nodes="nodes"
         :edges="edges"
         :options="options"
+        @click="onNodeSelected"
       >
       </network>
 
+      <div class="mt-4">
+        <v-btn @click="hierarchical = false">hierarchicalを使用しない</v-btn>
+        <v-btn @click="hierarchical = true">hierarchicalを使用する</v-btn>
+
+        <v-btn @click="sortMethod = 'directed'">directed</v-btn>
+        <v-btn @click="sortMethod = 'hubsize'">hubsize</v-btn>
+
+        <v-btn @click="physicsEnabled = true">physicsを使用する</v-btn>
+        <v-btn @click="physicsEnabled = false">physicsを使用しない</v-btn>
+      </div>
       <hr class="mt-10" />
 
       <h1 class="mt-10">以下はメモ</h1>
@@ -117,25 +128,11 @@ export default {
 
       nodes: [],
       edges: [],
-      options: {
-        edges: {
-          length: 400, // Longer edges between nodes.
-        },
-        layout: {
-          randomSeed: 2,
-        },
-        physics: {
-          // enabled: false,
-          // Even though it's disabled the options still apply to network.stabilize().
-          /*
-          enabled: false,
-          solver: 'repulsion',
-          repulsion: {
-            nodeDistance: 4000, // Put more distance between the nodes.
-          },
-          */
-        },
-      },
+      nodesMap: {},
+
+      sortMethod: 'directed',
+      hierarchical: false,
+      physicsEnabled: true,
     }
   },
   computed: {
@@ -157,6 +154,40 @@ export default {
           text: 'Facet ' + this.id,
         },
       ]
+    },
+    options() {
+      const hierarchical = this.hierarchical
+      let hierarchicalOption = false
+      if (hierarchical) {
+        hierarchicalOption = {
+          sortMethod: this.sortMethod,
+        }
+      }
+
+      const option = {
+        edges: {
+          length: 400, // Longer edges between nodes.
+        },
+        layout: {
+          randomSeed: 2,
+          hierarchical: hierarchicalOption,
+        },
+        physics: {
+          enabled: this.physicsEnabled,
+          // Even though it's disabled the options still apply to network.stabilize().
+          /*
+          enabled: false,
+          solver: 'repulsion',
+          repulsion: {
+            nodeDistance: 4000, // Put more distance between the nodes.
+          },
+          */
+        },
+      }
+
+      console.log({ option })
+
+      return option
     },
   },
   mounted() {
@@ -265,12 +296,24 @@ export default {
       const query = `prefix ex: <https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#>
       prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
       SELECT * WHERE {
-        ?s ?p ?o; ex:description ?desc_s . 
-        optional { ?s ?related_o ?s_o . ?s_o ex:referencesEntityInContext ?entityInContext_s } 
+        ?s ?p ?o; ex:description ?desc_s .
+        optional {
+          ?s ex:referencesEntityInContext ?entityInContext_s0; ex:referencesEntity ?referencesEntity_s0 .
+          ?referencesEntity_s0 ex:name ?referencesEntityName_s0; rdf:type ?referencesEntityType_s0
+        }
+        optional {
+          ?s ?related_so ?s_o .
+          ?s_o ex:referencesEntityInContext ?entityInContext_s; ex:referencesEntity ?referencesEntity_s .
+          ?referencesEntity_s ex:name ?referencesEntityName_s; rdf:type ?referencesEntityType_s
+        }
         ?o a/rdfs:subClassOf* ex:Factoid
         filter (?s = <${item.s}> || ?o = <${item.s}>)
         ?o ex:description ?desc_o .
-        optional { ?o ?related_o ?r_o . ?r_o ex:referencesEntityInContext ?entityInContext_o }
+        optional {
+          ?o ?related_oo ?o_o .
+          ?o_o ex:referencesEntityInContext ?entityInContext_o; ex:referencesEntity ?referencesEntity_o .
+          ?referencesEntity_o ex:name ?referencesEntityName_o; rdf:type ?referencesEntityType_o
+        }
       }`
 
       const url = `${endpoint}?query=${encodeURIComponent(query)}`
@@ -283,6 +326,13 @@ export default {
       for (const obj of data) {
         const keys = ['s', 'o']
         for (const key of keys) {
+          // entity
+          const entityNodeUri = obj[`referencesEntity_${key}`]
+
+          if (!entityNodeUri) {
+            continue
+          }
+
           const nodeUri = obj[key]
 
           const rna = obj[`desc_${key}`]
@@ -297,7 +347,70 @@ export default {
               id: nodeUri,
               label: cdn2.join('\n'),
               shape: 'box',
+              color: {
+                background: 'white',
+              },
+              type: 'factoid',
             }
+          }
+
+          if (!nodesMap[entityNodeUri]) {
+            let color = null
+            let shape = null
+            const entityType = obj[`referencesEntityType_${key}`]
+            switch (entityType) {
+              case 'https://github.com/johnBradley501/FPO/raw/master/fpo.owl#Location':
+                // 式の結果が value1 に一致する場合に実行する文
+                color = '#98fb98' // 'green'
+                break
+              case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#Community':
+                // 式の結果が value1 に一致する場合に実行する文
+                color = 'red'
+                shape = 'square'
+                break
+              case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#ConceptualObjectReference':
+                // 式の結果が value1 に一致する場合に実行する文
+                color = 'yellow'
+                shape = 'diamond'
+                break
+              case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#PhysicalObjectReference':
+                // 式の結果が value1 に一致する場合に実行する文
+                color = 'yellow'
+                shape = 'diamond'
+                break
+              /*
+              case value2:
+                // 式の結果が value2 に一致する場合に実行する文
+                [break;]
+              ...
+              case valueN:
+                // 式の結果が valueN に一致する場合に実行する文
+                [break;]
+              [default:
+                // 式の値に一致するものが存在しない場合に実行する文
+                [break;]]
+              */
+            }
+            nodesMap[entityNodeUri] = {
+              id: entityNodeUri,
+              label: obj[`referencesEntityName_${key}`],
+              // shape: 'box',
+              color,
+              shape,
+              context: obj[`entityInContext_${key}`],
+              type: entityType,
+            }
+          }
+
+          // factoid と entity をつなぐ edge
+          const edgeUri4f2e = `${nodeUri}-${entityNodeUri}`
+          edgesMap[edgeUri4f2e] = {
+            from: nodeUri,
+            to: entityNodeUri,
+            // label: this.$utils.getIdFromUri(obj.p, '#'),
+            arrows: 'to',
+            // font: { align: 'middle' },
+            color: 'gray',
           }
         }
 
@@ -312,6 +425,7 @@ export default {
             label: this.$utils.getIdFromUri(obj.p, '#'),
             arrows: 'to',
             // font: { align: 'middle' },
+            color: 'gray',
           }
         }
       }
@@ -328,6 +442,40 @@ export default {
 
       this.edges = edges
       this.nodes = nodes
+
+      console.log({ edges, nodes })
+
+      this.nodesMap = nodesMap
+    },
+
+    // クリックした時の処理
+    onNodeSelected(value) {
+      const nodes = value.nodes
+
+      if (nodes.length > 0) {
+        const uri = nodes[0]
+        const node = this.nodesMap[uri]
+
+        if (node.type === 'factoid') {
+          this.$router.push(
+            this.localePath({
+              name: 'item-id',
+              params: {
+                id: this.$utils.getIdFromUri(node.id).replace('fact_', ''),
+              },
+            })
+          )
+        } else {
+          this.$router.push(
+            this.localePath({
+              name: 'entity-id',
+              params: {
+                id: this.$utils.getIdFromUri(node.context),
+              },
+            })
+          )
+        }
+      }
     },
   },
 }
